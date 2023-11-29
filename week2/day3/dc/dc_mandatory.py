63a61cadcb4e99479e222fd5e482e3030295634d
Avi748 <avrahm748@gmail.com>
2023-11-29 09:30:28 +0200

    update

---
+++ week2/day3/dc/dc_mandatory.py
@@ -0,0 +1,8 @@
+from collections import defaultdict
+#1
+# user_input = str(input())
+# dic = defaultdict(list)
+# for key, value in enumerate(user_input):
+#     dic[value].append(key)
+# print(dic)
+
--- week2/day3/ex_class.py
+++ week2/day3/ex_class.py
@@ -26,3 +26,20 @@ for i in keys_to_remove:
     del sample_dict[i]
 
 print(sample_dict)
+
+# #2
+# wallet = "$300"
+# items_purchase = {
+#   "Water": "$1",
+#   "Bread": "$3",
+#   "TV": "$1,000",
+#   "Fertilizer": "$20"
+# }
+# new_list_keys = list(items_purchase.keys())
+# new_list_value = list(items_purchase.values())
+# print(len(wallet))
+# print(len(new_list_value[0]))
+
+# for i in new_list_value:
+#     i = new_list_value[i]
+# print(i)
